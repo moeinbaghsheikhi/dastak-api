463bec5eb6e3b14ce8e3fa2627aa3fcbd3066ff3
moein baghsheikhi <moein.baghsheikhi@gmail.com>
2023-09-08 20:01:26 +0330

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder , SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
